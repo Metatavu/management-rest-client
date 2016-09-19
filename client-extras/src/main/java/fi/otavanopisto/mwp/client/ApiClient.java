bd9f79c25bfaa338da0dde70e75e84eeb4485b9f
Antti Leppä <antti.leppa@foyt.fi>
2016-09-19 13:31:46 +0300

    Refactored api classes to accept baseUrl parameter

--- client-extras/src/main/java/fi/otavanopisto/mwp/client/ApiClient.java
+++ client-extras/src/main/java/fi/otavanopisto/mwp/client/ApiClient.java
@@ -4,12 +4,12 @@ import java.util.Map;
 
 public interface ApiClient {
   
-  public abstract <T> ApiResponse<T> doGETRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  public <T> ApiResponse<T> doGETRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
   
-  public abstract <T> ApiResponse<T> doPOSTRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  public <T> ApiResponse<T> doPOSTRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
   
-  public abstract <T> ApiResponse<T> doPUTRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  public <T> ApiResponse<T> doPUTRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
   
-  public abstract <T> ApiResponse<T> doDELETERequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  public <T> ApiResponse<T> doDELETERequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
   
 }
