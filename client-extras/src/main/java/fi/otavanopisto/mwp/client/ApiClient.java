726aef3488a2a5bac303110710bacb0c20099a24
Antti Leppä <antti.leppa@foyt.fi>
2016-09-19 13:07:32 +0300

    Changed ApiClient paths to full path
    And refactored ApiClient to be interface

--- client-extras/src/main/java/fi/otavanopisto/mwp/client/ApiClient.java
+++ client-extras/src/main/java/fi/otavanopisto/mwp/client/ApiClient.java
@@ -2,14 +2,14 @@ package fi.otavanopisto.mwp.client;
 
 import java.util.Map;
 
-public abstract class ApiClient {
+public interface ApiClient {
   
-  public abstract <T> ApiResponse<T> doGETRequest(String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  public abstract <T> ApiResponse<T> doGETRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
   
-  public abstract <T> ApiResponse<T> doPOSTRequest(String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  public abstract <T> ApiResponse<T> doPOSTRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
   
-  public abstract <T> ApiResponse<T> doPUTRequest(String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  public abstract <T> ApiResponse<T> doPUTRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
   
-  public abstract <T> ApiResponse<T> doDELETERequest(String path, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  public abstract <T> ApiResponse<T> doDELETERequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
   
 }
