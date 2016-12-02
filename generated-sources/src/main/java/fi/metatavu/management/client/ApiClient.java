29c55b443116e9819de139430bdb6209961aebdd
Antti Leppä <antti.leppa@metatavu.fi>
2016-12-02 14:07:20 +0200

    Generated source

---
+++ generated-sources/src/main/java/fi/metatavu/management/client/ApiClient.java
@@ -0,0 +1,15 @@
+package fi.metatavu.management.client;
+
+import java.util.Map;
+
+public interface ApiClient {
+  
+  public <T> ApiResponse<T> doGETRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  
+  public <T> ApiResponse<T> doPOSTRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  
+  public <T> ApiResponse<T> doPUTRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  
+  public <T> ApiResponse<T> doDELETERequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  
+}
