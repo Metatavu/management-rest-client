563b68997f31759a3816a65a3aa05e7fe3169080
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-05-05 14:40:27 +0300

    Updated client to support HEAD requests

--- client-extras/src/main/java/fi/metatavu/management/client/ApiClient.java
+++ client-extras/src/main/java/fi/metatavu/management/client/ApiClient.java
@@ -6,6 +6,8 @@ public interface ApiClient {
   
   public <T> ApiResponse<T> doGETRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
   
+  public <T> ApiResponse<T> doHEADRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
+  
   public <T> ApiResponse<T> doPOSTRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
   
   public <T> ApiResponse<T> doPUTRequest(String url, ResultType<T> resultType, Map<String, Object> queryParams, Map<String, Object> postParams);
